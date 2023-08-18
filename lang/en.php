2324fff6f85ef20c89e1b8d7cc71b5664e357a9a
Philipp <job@umk0.ru>
2023-08-19 00:17:40 +0300

    add lang

---
+++ lang/en.php
@@ -0,0 +1,27 @@
+<?
+$lang = [
+     "welcome_message" => "Hello friend here's what I can do:",
+     "back_btn" => "Back",
+     "status_server_btn" => "Server status",
+     "pay_menu_btn" => "Available services",
+     "pay_bank_card_btn" => "Pay by bank card",
+     "pay_yoomoney_btn" => "Pay from YooMoney balance",
+     "service_message" => "Here are the services I can offer:",
+     "service_term" => "Select the desired term",
+     "service_back" => "We can go back",
+     "service_no_desc" => "No description",
+     "service_title" => "%service_name% %days_text% - %price_text%", //available %service_name% - service title %days_text% - number of days with designation text (1 day), %days% - only number of days, % price_text% - price with designation text (1 ruble), %price% - price only, %flags% - flags provided by the service
+     "service_desc" => "%service_desc%\nFlags: %flags%", //available %service_name% - service name, %service_desc% - service description, %days_text% - number of days with designation text (1 day), %days % - only the number of days, %price_text% - cost with the designation text (1 ruble), %price% - only the cost, %flags% - flags that the service gives
+     "day_1" => "day",
+     "day_234" => "days",
+     "day_n" => "days",
+     "currency_1" => "ruble",
+     "currency_234" => "ruble",
+     "currency_n" => "rubles",
+     "map" => "Map",
+     "players" => "Players",
+     "server_off" => "Server not available",
+     "main_menu_btn" => "Main menu",
+     "buy_more_btn" => "Buy more",
+     "pay_success_message" => "Payment for service %service_name%\nSuccessful\nCommand for in-game console:\nvk_promo %promocode%" // available %service_name% - service name, %service_desc% - service description, %flags% - flags provided by the service, %promocode% - generated promo code
+];
---
+++ lang/ua.php
@@ -0,0 +1,27 @@
+<?
+$lang = [
+     "welcome_message" => "Привіт друг ось що я вмію:",
+     "back_btn" => "Назад",
+     "status_server_btn" => "Статус сервера",
+     "pay_menu_btn" => "Доступні послуги",
+     "pay_bank_card_btn" => "Оплата банківською карткою",
+     "pay_yoomoney_btn" => "Оплата з балансу YooMoney",
+     "service_message" => "Ось які послуги я можу запропонувати:",
+     "service_term" => "Виберіть потрібний термін",
+     "service_back" => "Можемо повернутися назад",
+     "service_no_desc" => "Немає опису",
+     "service_title" => "%service_name% %days_text% - %price_text%", //доступні %service_name% - назва усулги %days_text% - кількість днів з текстом позначення (1 день), %days% - лише кількість днів, % price_text% - вартість з текстом позначення (1 рубль), %price% - тільки вартість, %flags% - прапори які надає послуга
+     "service_desc" => "%service_desc%\nПрапори: %flags%", //доступні %service_name% - назва послуги, %service_desc% - опис послуги, %days_text% - кількість днів з текстом позначення (1 день), %days % - лише кількість днів, %price_text% - вартість з текстом позначення (1 рубль), %price% - лише вартість, %flags% - прапори які надає послуга
+     "day_1" => "день",
+     "day_234" => "дня",
+     "day_n" => "днів",
+     "currency_1" => "рубль",
+     "currency_234" => "рубля",
+     "currency_n" => "рублів",
+     "map" => "Карта",
+     "players" => "Гравців",
+     "server_off" => "Сервер недоступний",
+     "main_menu_btn" => "Головне меню",
+     "buy_more_btn" => "Купити ще",
+     "pay_success_message" => "Оплата за послугою %service_name%\nПройшла успішно\nКоманда для консолі в грі:\nvk_promo %promocode%" //доступні %service_name% - назва послуги, %service_desc% - опис послуги, %flags% - прапори які надає послуга, %promocode% - згенерований промокод
+];
