1378e9992e6e8e800f14efb81854bad259ce7505
Philipp <job@umk0.ru>
2023-08-19 00:10:46 +0300

    test pull

--- bootstrap.php
+++ bootstrap.php
@@ -1,14 +1,15 @@
 <?
 require_once 'setting.dev.php';
-if(file_exists("lang/".$setting["LANG"].".php")){
-    require_once "lang/".$setting["LANG"].".php";
-}else{
+if (file_exists("lang/" . $setting["LANG"] . ".php")) {
+    require_once "lang/" . $setting["LANG"] . ".php";
+} else {
     require_once "lang/ru.php";
 }
 require_once "vendor/autoload.php";
 require_once 'vk.php';
 require_once 'CodeGenerator.php';
 require_once 'function.php';
+
 use Medoo\Medoo;
 use xPaw\SourceQuery\SourceQuery;
 
@@ -20,7 +21,7 @@ $DB = new Medoo([
     'database_name' => $setting["MYSQL"]["database"],
     'username' => $setting["MYSQL"]["username"],
     'password' => $setting["MYSQL"]["password"],
-    'prefix' => $setting["MYSQL"]["prefix"]."_"
+    'prefix' => $setting["MYSQL"]["prefix"] . "_"
 ]);
 $Query = new SourceQuery();
 $data = json_decode(file_get_contents('php://input'), true);
